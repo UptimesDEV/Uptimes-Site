4f7273c247c7f4bfad77285090c8f15ce38bff8b
UptimesDEV <uptimesdevelopment@gmail.com>
2024-12-07 00:36:20 +0300

    Delete pages directory

--- pages/500.jsx
+++
@@ -1,6 +0,0 @@
-import ErrorPage from '../components/ErrorPage';
-const UnkownPage = () => {
-    return <ErrorPage code={500} message={"Oh. Sanırım 500 İç Sunucu Hatası var. Lütfen daha sonra tekrar deneyin!"} />
-}
-
-export default UnkownPage;
