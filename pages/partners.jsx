0dda82274af0dd03b514985548a49b12bdb9a832
UptimesDEV <uptimesdevelopment@gmail.com>
2024-12-07 00:40:13 +0300

    Add files via upload

---
+++ pages/partners.jsx
@@ -0,0 +1,40 @@
+
+export default function Partners() {
+
+    return (
+        <>
+            <div className="w-full my-10">
+            <div className="flex w-full justify-center">
+      <img width="400" src="/img/bck4.png" />
+      </div>
+                <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
+                    <i className="fal fa-stars text-amber-400 mr-2" />
+                    Partnerler
+                </h1>
+            </div>
+
+            <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+
+            <div className="flex flex-col justify-center text-white rounded">
+            <a href="">
+                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
+                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/attachments/1215660447626301550/1314705957162319953/DALLE_2024-11-17_00.52.45_-_A_modern_logo_design_with_the_letters_UT_prominently_displayed_in_bold_clean_typography._The_background_features_a_rainy_atmosphere_with_realistic_.webp?ex=6754bed6&is=67536d56&hm=dc4c4eb75d91a89eb99f1697b6e5a229a3d3a2cc0fc2c6670801f277a2808bcd&" />
+                            <div>
+                                <h1 className="leading-none text-3xl font-bold text-white">Uptimes Development</h1>        
+                                <div className="flex items-center mt-1">
+                                    <p className="font-normal font-sm">
+                                    </p>
+                                </div>     
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+
+
+                           
+
+                </div>
+                <div className="py-10"></div>
+        </>
+    );
+};
